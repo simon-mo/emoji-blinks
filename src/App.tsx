e43a792703a55eef04e7a890999dda6879a817c8
Simon Mo <xmo@berkeley.edu>
2019-09-22 21:17:04 -0700

    Implement features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import posed from "react-pose";
+import * as _ from "lodash";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const Box = posed.p({
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 }
+});
+
+let emojiSelection = _.range(128000, 128064).map(int =>
+  String.fromCodePoint(int)
+);
+
+class App extends React.Component {
+  state = { isVisible: true };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
+    }, 300);
+  }
+
+  render() {
+    const { isVisible } = this.state;
+    const emoji = isVisible
+      ? emojiSelection[_.random(0, emojiSelection.length)]
+      : "";
+
+    return (
+      <div>
+        <h1> Zoooooooooo </h1>
+        <Box
+          style={{ fontSize: "100px" , paddingLeft: "50px"}}
+          pose={isVisible ? "visible" : "hidden"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {" "}
+          {emoji}{" "}
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default App;
